7bfcbea5e920ea0380e651622c46a33322c09aad
bhbmaster <bhbmaster@gmail.com>
2020-12-01 11:07:51 -0800

    thinking about data structure in TODO.py - its just comments in there

---
+++ TODO.py
@@ -0,0 +1,10 @@
+# TODO: to figure out stock profit make a class called stock-orders
+# stockname: string
+# orders: dictionary list of all the orders { date: datetime, type: string buy or sell, price: float, amount: float }
+# or orders can be a list of all order instanstance class (then we would need to create an order class)
+
+# then have a list of all of the stocks
+# stocks = [ stock-orders from APPL, stock-orders from TSLA, etc...]
+
+# order class:
+# input: date string (in class can convert to datetime), type: string buy or sell, price: float, amount: float
