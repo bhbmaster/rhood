34235115b395b4be0e1a3b97d8fa3345f2af4a23
bhbmaster <bhbmaster@gmail.com>
2022-03-13 22:37:45 +0100

    modified run.sh to dump to subdirs in archive. archive/output and archive/dat

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@
 # make a dated copy into archives dir as well
 DATE=$(/bin/date +%Y-%m-%d-%H%M)
 mkdir archive 2> /dev/null
+mkdir archive/output 2> /dev/null
+mkdir archive/dat 2> /dev/null
 # more info:
 python rhood.py --all-info --extra --save --csv --profile-csv > output.txt 2>&1
 # less info:
 # python rhood.py --finance-info --extra --save --csv > output.txt 2>&1
-/bin/cp "output.txt" "archive/output-$DATE.txt"
-/bin/cp "dat.pkl" "archive/dat-$DATE.pkl"
+/bin/cp "output.txt" "archive/output/output-$DATE.txt"
+/bin/cp "dat.pkl" "archive/dat/dat-$DATE.pkl"
