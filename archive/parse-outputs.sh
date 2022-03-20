7095410f9314d41fb6ba42108033b488be2685f9
bhbmaster <bhbmaster@gmail.com>
2022-03-19 19:18:43 -0700

    fixed chmod to 755 on many files. also added code to parse output results in archive/output showing oneline per run in chorological order with archive/parse-outputs.sh. also added archive/rotate.sh file, which compresses dat and output files with xz. the parse code is smart enough to first parse the compressed rotated logs and then show the uncompressed ones.

---
+++ archive/parse-outputs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Last update: 2022-03-19
+#
+# filename: rhood/archive/parse-output.sh
+# what this does: shows oneline info of profits from each run of rhood
+# format of output:
+# 2022-03-19 17:01:55  -  * STOCK total net profit $3318.65 * CRYPTO total net profit $1884.15 * total profit from stocks, crypto, and options + dividends: $5227.43
+# 2022-03-19 18:01:56  -  * STOCK total net profit $3318.65 * CRYPTO total net profit $1884.15 * total profit from stocks, crypto, and options + dividends: $5227.43
+#
+SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"  # we need to get to output dir in SCRIPT_DIR, so first make sure we are there
+cd output;
+#-------------------------------- analyze old files --------------------------#
+# these ones are txt compressed xz files created by rotate.sh, so we have to extract them in chronological order
+# the filename (which has the date) is prefixed to everyline, which makes it useful
+# during this extraction process.
+# the files have the date in yyyy-mm-dd order, so they are read in order from oldest to newest
+# also within eachfile they are ordered from oldest to newest. thus order is always maintained from oldest to newest.
+CONCAT="";
+OLD_INPUT_DATE=""
+N=0 # hack to not print first useless line
+xzcat $(ls -1tr output*txt.xz) | \
+grep "total.*profit" | \
+grep -v "total net profit from stocks" | while read i; do
+	((N++))
+	INPUT_DATE=$(echo "$i" | cut -d: -f1 | cat -e);
+	if [[ $N -gt 1 ]]; then
+	  if [[ "$OLD_INPUT_DATE" != "$INPUT_DATE" ]]; then
+	  	MOD_DATE=$(echo "$OLD_INPUT_DATE" | sed 's|output-||g;s|.txt\$||g' | sed 's|\([0-9]*-[0-9]*-[0-9]*\)-\([0-9][0-9]\)\([0-9][0-9]\)|\1 \2:\3:##|')
+	 	echo "${MOD_DATE}  - ${CONCAT}" | sed 's|\^M\$||g';
+	 	CONCAT="";
+	  fi
+	fi
+	REMAINING=$(echo "$i" | cut -d: -f2- | cat -e);
+	CONCAT="${CONCAT}${REMAINING}";
+	OLD_INPUT_DATE=$INPUT_DATE;
+done;
+#-------------------------------- analyze current files --------------------------#
+# cat -A is needed as there are weird meta ^M characters that throw off the newline output
+# we then use sed to clean up the ^M and $ (at end of string) occurances
+ls -1tr output*txt | while read i; do
+	set -f # so that * are not expanded. we don't want it at the top of script as then other stuff might not work and a while loop is in a way a subshell
+	echo $( stat -c "%w" $i | cut -f1 -d"."; ) " - " $( cat $i | grep "total.*profit" | grep -v "total net profit from stocks"; );
+done | grep -v " - $" | cat -A | sed 's|\^M||g' | sed 's|$$||g'
+#-------------------------------- rotate old files --------------------------#
+# ./rotate.sh # <--- only do this if we run analyze like once a week or something (so that we have some good amount of files to rotate)
+exit 0
+# EOF
---
+++ archive/rotate.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# file: rotate.sh
+
+DATESUF=`date +%Y%m%d-%H%M%S`
+LOG_OUTPUT="output/rotate-$DATESUF.log"
+LOG_DAT="dat/rotate-$DATESUF.log"
+
+### compress and rotate output files - run from archive dir and it creates an output-from-DATE-to-DATE.txt.zx file in archive/output from all of the output*txt files in output
+( FILENAMEPREFIX="output";
+cd output
+LSOUT=$(ls -1tr | grep "^${FILENAMEPREFIX}.*txt$")
+LENGTH=$(echo "$LSOUT" | grep -c .)
+FIRSTDATE=$(echo "$LSOUT" | grep . | head -1 | sed 's|output-||g;s|\.txt||g')
+LASTDATE=$(echo "$LSOUT" | grep . | tail -1 | sed 's|output-||g;s|\.txt||g')
+FINALFILE="output-from-$FIRSTDATE-to-$LASTDATE.txt"
+N=0
+echo "** `date` starting output files rotate:"
+echo "** found $LENGTH files to rotate/compress"
+[[ "$LENGTH" -le 2 ]] && { echo "** need 3 or more files to rotate. exiting."; exit 3; }
+for i in $(echo "$LSOUT" | grep . | grep -v "$FINALFILE"); do
+	((N++))
+	echo "processed $N/$LENGTH $i into $FINALFILE" >&2  # progress of appending file content w/ date prefix to single file (this is not appended) as its stderr
+	cat $i | awk -v PREFIX=$i '{print PREFIX": "$0}' # this is appended into the file as its stdout
+done > ${FINALFILE} # since we are only redirecting stdout we get the benefit of having a progress output
+echo "** `date` concatted $FINALFILE:"
+ls -lhtr ${FINALFILE}
+echo "** `date` xzing the file:"
+xz -9 ${FINALFILE}
+echo "** `date` final xzed file:"
+ls -lhtr ${FINALFILE}*
+# delete logic
+echo "** `date` deleting original files:"
+echo "$LSOUT" | grep . | while read i; do rm -vf "$i"; done
+# echo "** `date` moving up one dir to archive/:"
+# mv -v ${FINALFILE}* ..
+echo "** `date` done"
+) &> "$LOG_OUTPUT" &
+# ) |& tee "notes-testing-xz-on-outputs-`date +%Y%m%d-%H%M%S`.out"
+
+### compress and rotate dat files - run from archive dir and it creates a dat-from-DATE-to-DATE.tar.xz file in archive/dat from all of the dat*pkl files in output
+( FILENAMEPREFIX="dat";
+cd dat
+LSOUT=$(ls -1tr | grep "^${FILENAMEPREFIX}.*pkl$")
+LENGTH=$(echo "$LSOUT" | grep -c .)
+FIRSTDATE=$(echo "$LSOUT" | grep . | head -1 | sed 's|dat-||g;s|\.pkl||g')
+LASTDATE=$(echo "$LSOUT" | grep . | tail -1 | sed 's|dat-||g;s|\.pkl||g')
+FINALFILE="dat-from-$FIRSTDATE-to-$LASTDATE.pkl.tar.xz"
+echo "** `date` starting dat files rotate:"
+echo "** found $LENGTH files to rotate/compress"
+[[ "$LENGTH" -le 2 ]] && { echo "** need 3 or more files to rotate. exiting."; exit 3; }
+echo "** `date` creating $FINALFILE from $LENGTH dat.*pkl files:"
+tar -Jcvvvf ${FINALFILE} ${FILENAMEPREFIX}*pkl | awk -v LENGTH=$LENGTH '{n+=1;print $0 "\t" n"/"LENGTH;}'
+echo "** `date` final xzed file:"
+ls -lhtr ${FINALFILE}
+echo "** `date` done"
+# # delete logic
+echo "** `date` deleting original files:"
+echo "$LSOUT" | grep . | while read i; do rm -vf "$i"; done
+# echo "** `date` moving up one dir to archive/:"
+# mv -v ${FINALFILE} ..
+echo "** `date` done"
+) &> "$LOG_DAT" &
+# ) |& tee "notes-testing-xz-on-dat-`date +%Y%m%d-%H%M%S`.out"
+
+echo "* Creating $LOG_OUTPUT can follow: tail -F $LOG_OUTPUT"
+echo "* Creating $LOG_DAT can follow: tail -F $LOG_DAT"
+
+exit 0
