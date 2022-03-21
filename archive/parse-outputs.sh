9259454a72018ee3dcc975ec0d8b0d8dd359850b
bhbmaster <bhbmaster@gmail.com>
2022-03-20 18:17:23 -0700

    fixed parse-output.sh not showing last compressed result. also changed compressed ones to be XX for seconds, and nonecompressed new ones to be ## for seconds.

--- archive/parse-outputs.sh
+++ archive/parse-outputs.sh
@@ -18,42 +18,52 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"  # we need to get to output dir in SCRIPT_DIR, so first make sure we are there
 cd output;
 
+SHOW_OLD=1 # 0 no 1 yes
+SHOW_NEW=1 # 0 no 1 yes
+
 #-------------------------------- analyze old files --------------------------#
 # these ones are txt compressed xz files created by rotate.sh, so we have to extract them in chronological order
 # the filename (which has the date) is prefixed to everyline, which makes it useful
 # during this extraction process.
 # the files have the date in yyyy-mm-dd HH:MM order, so they are read in order from oldest to newest
 # also within eachfile they are ordered from oldest to newest. thus order is always maintained from oldest to newest.
-# NOTE: when these compressed files are analyzed their dates are written in format yyyy-mm-dd HH:MM:## as that is all we keep in the filename
-CONCAT="";
-OLD_INPUT_DATE=""
-N=0 # hack to not print first useless line
-xzcat $(ls -1tr output*txt.xz) | \
-grep "total.*profit" | \
-grep -v "total net profit from stocks" | while read i; do
-	((N++))
-	INPUT_DATE=$(echo "$i" | cut -d: -f1 | cat -e);
-	if [[ $N -gt 1 ]]; then
-	  if [[ "$OLD_INPUT_DATE" != "$INPUT_DATE" ]]; then
-	  	MOD_DATE=$(echo "$OLD_INPUT_DATE" | sed 's|output-||g;s|.txt\$||g' | sed 's|\([0-9]*-[0-9]*-[0-9]*\)-\([0-9][0-9]\)\([0-9][0-9]\)|\1 \2:\3:##|')
-	 	echo "${MOD_DATE}  - ${CONCAT}" # | sed 's|\^M\$||g';
-	 	CONCAT="";
-	  fi
-	fi
-	REMAINING=$(echo "$i" | cut -d: -f2- | cat -A | sed 's|\^M||g' | sed 's|$$||g');
-	# echo "(R $REMAINING)"
-	CONCAT="${CONCAT}${REMAINING}";
-	OLD_INPUT_DATE=$INPUT_DATE;
-done;
+# NOTE: when these compressed files are analyzed their dates are written in format yyyy-mm-dd HH:MM:XX as that is all we keep in the filename
+if [[ "$SHOW_OLD" ==  1 ]]; then
+	CONCAT="";
+	OLD_INPUT_DATE=""
+	xzcat $(ls -1tr output*txt.xz) 2> /dev/null | \
+	# FOR TEST # xzcat output-from-2022-03-20-0200-to-2022-03-20-1700.txt.xz 2> /dev/null | \ 
+	grep "total.*profit" | \
+	grep -v "total net profit from stocks" | while read i; do
+		# ((N++))
+		INPUT_DATE=$(echo "$i" | cut -d: -f1 | cat -e);
+		REMAINING=$(echo "$i" | cut -d: -f2- | cat -A | sed 's|\^M||g' | sed 's|$$||g');
+		# echo "(R $REMAINING)"
+		CONCAT="${CONCAT}${REMAINING}";
+		OLD_INPUT_DATE=$INPUT_DATE;
+		# we can print out the results if we encountered the last sentence we need to see
+		if echo "$REMAINING" | grep -q "total profit from stocks"; then
+			# echo "LAST ONE:"
+			MOD_DATE=$(echo "$INPUT_DATE" | sed 's|output-||g;s|.txt\$||g;s|\.txt||g' | sed 's|\([0-9]*-[0-9]*-[0-9]*\)-\([0-9][0-9]\)\([0-9][0-9]\)|\1 \2:\3:XX|')
+	        echo "${MOD_DATE}  - ${CONCAT}" # | sed 's|\^M\$||g';
+	        CONCAT=""
+		fi
+	done;
+fi
+
 
 #-------------------------------- analyze current files --------------------------#
 # cat -A is needed as there are weird meta ^M characters that throw off the newline output
 # we then use sed to clean up the ^M and $ (at end of string) occurances
-# NOTE: when these uncompressed files are analyzed their dates are written in format yyyy-mm-dd HH:MM:SS from stat output
-ls -1tr output*txt | while read i; do
-	set -f # so that * are not expanded. we don't want it at the top of script as then other stuff might not work and a while loop is in a way a subshell
-	echo $( stat -c "%w" $i | cut -f1 -d"."; ) " - " $( cat $i | grep "total.*profit" | grep -v "total net profit from stocks"; );
-done | grep -v " - $" | cat -A | sed 's|\^M||g' | sed 's|$$||g'
+# NOTE: when these uncompressed files are analyzed their dates are written in format yyyy-mm-dd HH:MM:## from stat output
+if [[ "$SHOW_NEW" ==  1 ]]; then
+	ls -1tr output*txt 2> /dev/null | while read i; do
+		set -f # so that * are not expanded. we don't want it at the top of script as then other stuff might not work and a while loop is in a way a subshell
+		# MOD_DATE=$( stat -c "%w" $i | cut -f1 -d"."; )
+		MOD_DATE=$(echo "$i" | sed 's|output-||g;s|.txt\$||g;s|\.txt||g' | sed 's|\([0-9]*-[0-9]*-[0-9]*\)-\([0-9][0-9]\)\([0-9][0-9]\)|\1 \2:\3:##|')
+		echo  "$MOD_DATE - " $( cat $i | grep "total.*profit" | grep -v "total net profit from stocks"; );
+	done | grep -v " - $" | cat -A | sed 's|\^M||g' | sed 's|$$||g'
+fi
 
 #-------------------------------- rotate old files --------------------------#
 # ./rotate.sh # <--- only do this if we run analyze like once a week or something (so that we have some good amount of files to rotate)
