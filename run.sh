b384b2149052eea096a1365e2b34a53cba2097a9
bhbmaster <bhbmaster@gmail.com>
2024-05-04 00:11:49 -0700

    updated run.sh for python3.11

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# 
 # run rhood script and save its output & generate csvs. also save the orders to dat.pkl.
 # make a dated copy into archives dir as well
-DATE=$(/bin/date +%Y-%m-%d-%H%M)
-PYTHON="/cygdrive/c/Users/Admin/AppData/Local/Programs/Python/Python310/python.exe"
-# PYTHON=python3.10
+DATE=$(date +%Y-%m-%d-%H%M)
+# PYTHON="/cygdrive/c/Users/Admin/AppData/Local/Programs/Python/Python310/python.exe"
+PYTHON=python3.11
 mkdir archive 2> /dev/null
 mkdir archive/output 2> /dev/null
 mkdir archive/dat 2> /dev/null
@@ -11,5 +12,5 @@ mkdir archive/dat 2> /dev/null
 $PYTHON rhood.py --all-info --extra --save --csv --profile-csv > output.txt 2>&1
 # less info:
 # python rhood.py --finance-info --extra --save --csv > output.txt 2>&1
-/bin/cp "output.txt" "archive/output/output-$DATE.txt"
-/bin/cp "dat.pkl" "archive/dat/dat-$DATE.pkl"
+cp "output.txt" "archive/output/output-$DATE.txt"
+cp "dat.pkl" "archive/dat/dat-$DATE.pkl"
